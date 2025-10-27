a5c08ce5161d1e30632f8400d24aeda11befb7d5
AndersonMP <andersonfer20@gmail.com>
2025-10-26 21:11:54 -0500

    propiedas y plantillas

--- src/app/mi-perfil/mi-perfil.ts
+++ src/app/mi-perfil/mi-perfil.ts
@@ -7,8 +7,21 @@ import {Component} from '@angular/core';
 })
 
 export class MiPerfil {
+
+    public nombre: string;
+    public edad: number;
+    public ocupacion: string;
+    public direccion: string;
+    public telefono: string;
+
     constructor(){
-        alert("¡Bienvenido al perfil, estás dentro de la apliación!");
+        this.nombre = "Juan Pérez";
+        this.edad = 30;
+        this.ocupacion = "Desarrollador de Software";
+        this.direccion = "Av. Amazonas y Río Coca, Quito, Ecuador.";
+        this.telefono = "+593 999 999 999";
+
+        // alert("¡Bienvenido al perfil, estás dentro de la apliación!");
         console.log("Se cargó el componente perfil correctamente.")
     }
 }
--- src/app/producto/producto.ts
+++ src/app/producto/producto.ts
@@ -9,8 +9,17 @@ import { Component } from '@angular/core';
 )
 
 export class Producto {
+
+    public nombre: string;
+    public marca: string;
+    public precio: number;
+    public descipcion: string;
+
     constructor(){
+        this.nombre = "PC Portátil";
+        this.marca = "Asus";
+        this.precio = 457;
+        this.descipcion = "Laptop enfocado en ofimática y programación."
         console.log("Componente producto cargado correctamente");
-        alert("Hola")
     }
 }
