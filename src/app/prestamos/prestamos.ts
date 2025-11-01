17d9358cd7b6283f8959581fa2d6492e0f666856
AndersonMP <andersonfer20@gmail.com>
2025-11-01 16:52:47 -0500

    Plantillar @switch Angular

--- src/app/prestamos/prestamos.ts
+++ src/app/prestamos/prestamos.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
-import { CommonModule} from "@angular/common";
+import { CommonModule } from "@angular/common";
+
+enum TipoPrestamo {
+  Personal = 'Personal',
+  Hipotecario = 'Hipotecario',
+  Vehicular = 'Vehicular'
+}
+
+interface Prestamo {
+  id: number;
+  tipo: TipoPrestamo;
+  interes: number;
+}
 
 @Component({
   selector: 'app-prestamos',
@@ -8,22 +20,29 @@ import { CommonModule} from "@angular/common";
   templateUrl: './prestamos.html',
   styleUrl: './prestamos.css',
 })
+
 export class Prestamos {
-  web= "https://mercadolibre.com";
-  
+  web = "https://mercadolibre.com";
+  TipoPrestamo = TipoPrestamo;
   redirigir = false;
 
   prestamosList = [
-    {id: 1, monto: 400, estado: 'Aprobado'},
-    {id: 2, monto: 1500, estado: 'Pendiente'},
-    {id: 3, monto: 3000, estado: 'Aprobado'},
-    {id: 4, monto: 750, estado: 'Pendiente'},
-    {id: 5, monto: 4000, estado: 'Aprobado'},
-    {id: 6, monto: 800, estado: 'Pendiente'},
+    { id: 1, monto: 400, estado: 'Aprobado' },
+    { id: 2, monto: 1500, estado: 'Pendiente' },
+    { id: 3, monto: 3000, estado: 'Aprobado' },
+    { id: 4, monto: 750, estado: 'Pendiente' },
+    { id: 5, monto: 4000, estado: 'Aprobado' },
+    { id: 6, monto: 800, estado: 'Pendiente' },
 
   ]
 
-  cambiarRedirigir(){
+  prestamo: Prestamo = {
+    id: 1,
+    tipo: TipoPrestamo.Vehicular,
+    interes: 12.5
+  }
+
+  cambiarRedirigir() {
     this.redirigir = !this.redirigir;
   }
 }
--- src/app/usuario/usuario.ts
+++ src/app/usuario/usuario.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
+import { Ventas } from '../ventas/ventas';
 
 @Component({
   selector: 'app-usuario',
-  imports: [],
+  standalone: true,
+  imports: [Ventas],
   templateUrl: './usuario.html',
   styleUrl: './usuario.css',
 })
 export class Usuario {
+  
   web = "https://amazon.com";
+  
   redirigir = false;
+  
   marcasAutos = [
     {id: 1, nombre: 'Toyota'},
     {id: 2, nombre: 'Kia'},
@@ -16,6 +21,13 @@ export class Usuario {
     {id: 4, nombre: 'Mazda'},
     {id: 5, nombre: 'Nissan'},
   ];
+
+  usuario = {
+    id: 1,
+    nombre: "José",
+    nick: "jose123",
+    rol: "admin",
+  }
 
   ngOnInit(){
     this.marcasAutos = [];
