5099fc2ddd4acb1f14869c46fd9503de934716ba
AndersonMP <andersonfer20@gmail.com>
2025-11-01 17:16:05 -0500

    Angular Pipes

--- src/app/prestamos/prestamos.ts
+++ src/app/prestamos/prestamos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule } from "@angular/common";
+import { CommonModule, CurrencyPipe, PercentPipe, DatePipe, UpperCasePipe, TitleCasePipe } from "@angular/common";
 
 enum TipoPrestamo {
   Personal = 'Personal',
@@ -16,34 +16,31 @@ interface Prestamo {
 @Component({
   selector: 'app-prestamos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CurrencyPipe, PercentPipe, DatePipe, UpperCasePipe, TitleCasePipe],
   templateUrl: './prestamos.html',
   styleUrl: './prestamos.css',
 })
-
 export class Prestamos {
   web = "https://mercadolibre.com";
   TipoPrestamo = TipoPrestamo;
   redirigir = false;
 
   prestamosList = [
-    { id: 1, monto: 400, estado: 'Aprobado' },
-    { id: 2, monto: 1500, estado: 'Pendiente' },
-    { id: 3, monto: 3000, estado: 'Aprobado' },
-    { id: 4, monto: 750, estado: 'Pendiente' },
-    { id: 5, monto: 4000, estado: 'Aprobado' },
-    { id: 6, monto: 800, estado: 'Pendiente' },
-
-  ]
+    { id: 1, monto: 400, estado: 'Aprobado', fecha: new Date(2025, 0, 12) },
+    { id: 2, monto: 1500, estado: 'Pendiente', fecha: new Date(2025, 2, 5) },
+    { id: 3, monto: 3000, estado: 'Aprobado', fecha: new Date(2025, 4, 25) },
+    { id: 4, monto: 750, estado: 'Pendiente', fecha: new Date(2025, 6, 14) },
+    { id: 5, monto: 4000, estado: 'Aprobado', fecha: new Date(2025, 8, 30) },
+    { id: 6, monto: 800, estado: 'Pendiente', fecha: new Date(2025, 10, 3) },
+  ];
 
   prestamo: Prestamo = {
     id: 1,
     tipo: TipoPrestamo.Vehicular,
-    interes: 12.5
-  }
+    interes: 0.125,
+  };
 
   cambiarRedirigir() {
     this.redirigir = !this.redirigir;
   }
 }
-
--- src/app/usuario/usuario.ts
+++ src/app/usuario/usuario.ts
@@ -1,23 +1,31 @@
 import { Component } from '@angular/core';
+import { UpperCasePipe, LowerCasePipe, DatePipe, TitleCasePipe, CurrencyPipe, DecimalPipe } from '@angular/common';
 import { Ventas } from '../ventas/ventas';
 
 @Component({
   selector: 'app-usuario',
   standalone: true,
-  imports: [Ventas],
+  imports: [
+    Ventas,
+    UpperCasePipe,
+    LowerCasePipe,
+    DatePipe,
+    TitleCasePipe,
+    CurrencyPipe,
+    DecimalPipe
+  ],
   templateUrl: './usuario.html',
   styleUrl: './usuario.css',
 })
 export class Usuario {
   
   web = "https://amazon.com";
-  
   redirigir = false;
   
   marcasAutos = [
     {id: 1, nombre: 'Toyota'},
     {id: 2, nombre: 'Kia'},
-    {id: 3, nombre: 'Chevrolte'},
+    {id: 3, nombre: 'Chevrolet'},
     {id: 4, nombre: 'Mazda'},
     {id: 5, nombre: 'Nissan'},
   ];
@@ -28,6 +36,10 @@ export class Usuario {
     nick: "jose123",
     rol: "admin",
   }
+
+  hoy = new Date(); 
+  precio = 2549.99;
+  valorDecimal = 3.141592; 
 
   ngOnInit(){
     this.marcasAutos = [];
